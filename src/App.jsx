09bcb9589cacd9d9f30887c6d5b01d2da848e73d
Miha <stele1000@gmail.com>
2025-12-22 19:09:11 +0100

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ const Navigation = () => {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            Hire Me
+            Get in Touch
           </motion.a>
         </div>
 
